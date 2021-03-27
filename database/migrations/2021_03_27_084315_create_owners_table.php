4795b40adf8aeb0f35206e696c62f4745254946f
sumanahmed <suman777333@gmail.com>
2021-03-27 14:52:53 +0600

    Model, Migration & Controller added

---
+++ database/migrations/2021_03_27_084315_create_owners_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('car_type');
+            $table->string('model');
+            $table->string('year');
+            $table->string('reg_number');
+            $table->string('address');
+            $table->string('driver_name');
+            $table->string('driver_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
