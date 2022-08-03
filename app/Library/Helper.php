fd681ee9c0216b0d1f87a1efd3ffe327b9df37b5
sumanahmed <suman777333@gmail.com>
2022-08-03 21:08:26 +0600

    masking sms aded

--- app/Library/Helper.php
+++ app/Library/Helper.php
@@ -7,7 +7,8 @@ class Helper {
 
     public static function sendSMS($mobile, $msg) {
         $client = new Client(['verify' => false ]);
-        $client->request("GET", "http://66.45.237.70/api.php?username=01670168919&password=TVZMBN3D&number=". $mobile ."&message=".$msg);
+        // $client->request("GET", "http://66.45.237.70/api.php?username=01670168919&password=TVZMBN3D&number=". $mobile ."&message=".$msg);
+        $client->request("GET", "http://66.45.237.70/api.php?username=Autospire&password=K8N92GBY&number=". $mobile ."&message=".$msg);
     }
 
 }
